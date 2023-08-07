23137cdb6ab10436eb872fbbe81eb7cbd2af8c9b
Gee3eeo <isanborgerald@gmail.com>
2023-08-07 01:56:13 +0000

    Basics

---
+++ 0x00-ES6_basic/10-main.js
@@ -0,0 +1,9 @@
+import appendToEachArrayValue from "./10-loops.js";
+
+console.log(
+	appendToEachArrayValue(["appended", "fixed", "displayed"], "correctly-")
+);import appendToEachArrayValue from "./10-loops.js";
+
+console.log(
+	appendToEachArrayValue(["appended", "fixed", "displayed"], "correctly-")
+);
